dc73dd645047c6d37c8191c25fef01c3935e01b6
Himani Agrawal <himani93@gmail.com>
2018-04-08 11:23:25 +0530

    added taxi creation test

---
+++ fuber/tests/test_taxi.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+class TestTaxi(object):
+
+    def test_taxi_color(self):
+        pass
+
+    def test_taxi_license_no(self):
+        pass
+
+    def test_taxi_category(self):
+        pass
+
+    def test_taxi_available(self):
+        pass
