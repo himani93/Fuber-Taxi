06e3fd10a4f7aea081114e5da777d5e4d6ee1f6a
Himani Agrawal <himani93@gmail.com>
2018-04-08 23:47:56 +0530

    list riders, rider, create rider

--- fuber/app.py
+++ fuber/app.py
@@ -1,13 +1,23 @@
 import falcon
 
 from controller.taxi_app import TaxiApp
-
+from controller.rider_app import RiderApp
+# from controller.ride_app import RideApp
 
 api = application = falcon.API()
 taxi_app = TaxiApp()
-
+rider_app = RiderApp()
+# ride_app = RideApp()
 
 api.add_route("/taxis/", taxi_app)
 api.add_route("/taxis/{taxi_id}", taxi_app)
+
+api.add_route("/riders/", rider_app)
+api.add_route("/riders/{rider_id}", rider_app)
+
+# api.add_route("/riders/{rider_id}/rides", ride_app)
+
+# api.add_route("/riders/{rider_id}/rides/current", ride_app)
+# api.add_route("/riders/{rider_id}/rides/")
 
 # if __name__ == "__main__":
---
+++ fuber/controller/rider_app.py
@@ -0,0 +1,50 @@
+import falcon
+import json
+
+from data import RIDERS
+from models.rider import Rider
+from models.exceptions import *
+
+
+class RiderApp(object):
+    def _get_rider(self, rider_id):
+        rider = None
+
+        for rider_it in RIDERS:
+            if rider_it.id == rider_id:
+                rider = rider_it
+                break
+
+        return rider
+
+    def _get_all_riders(self):
+        return [rider.to_dict() for rider in RIDERS]
+
+    def on_get(self, request, response, rider_id=None):
+        if rider_id:
+            rider = self._get_rider(rider_id)
+            if not rider:
+                raise falcon.HTTPNotFound()
+
+            response.body = json.dumps(rider.to_dict())
+            response.status = falcon.HTTP_200
+        else:
+            riders = self._get_all_riders()
+
+            response.body = json.dumps({"riders": riders})
+            response.status = falcon.HTTP_200
+
+    def on_post(self, request, response):
+        # from pudb import set_trace; set_trace()
+        body = json.load(request.stream)
+        try:
+            rider = Rider(body.get("name"))
+        except InvalidRiderNameException as e:
+            response.body = json.dumps({"message": "Rider name is not valid"})
+            response.status = falcon.HTTP_400
+        else:
+            RIDERS.append(rider)
+            response.body = json.dumps({"message": "Rider registered.", "data": rider.to_dict()})
+            response.status = falcon.HTTP_201
+
+        print rider.id
