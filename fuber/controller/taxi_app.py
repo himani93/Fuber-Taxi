540350d2c261c565176e7281d8a844dca7af5650
Himani Agrawal <himani93@gmail.com>
2018-04-08 20:09:58 +0530

    register taxis via POST req

--- fuber/controller/taxi_app.py
+++ fuber/controller/taxi_app.py
@@ -2,7 +2,8 @@ import falcon
 import json
 
 from data import TAXIS
-from fuber.taxi import Taxi
+from models.taxi import Taxi
+from models.exceptions import *
 
 
 class TaxiApp(object):
@@ -11,12 +12,18 @@ class TaxiApp(object):
         response.status = falcon.HTTP_200
 
     def on_post(self, request, response):
-        body = request.stream.read()
+        body = json.load(request.stream)
         try:
             taxi = Taxi(body.get("license_no"), body.get("color"))
-        except Exception as e:
-            print e
+            print taxi.id
+        except InvalidTaxiColorException as e:
+            response.body = json.dumps({"message": "Taxi color is not valid"})
+            response.status = falcon.HTTP_400
+        except InvalidTaxiLicenseNumberException as e:
+            response.body = json.dumps({"message": "Taxi license number is not valid"})
             response.status = falcon.HTTP_400
         else:
             TAXIS.append(taxi)
+            print TAXIS
+            response.body = json.dumps({"message": "Taxi registered."})
             response.status = falcon.HTTP_201
